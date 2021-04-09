206604563f7b0ebf383bb3f1500288920008b8c2
955 <37462352+955a0@users.noreply.github.com>
2021-04-09 09:07:05 +0900

    Update Windows_IIS_Enable.ps1

--- Windows_IIS_Enable.ps1
+++ Windows_IIS_Enable.ps1
@@ -3,3 +3,4 @@ Get-WindowsFeature -Name Web-Server | Format-List -Property *
 
 # IISの有効化
 Install-WindowsFeature Web-Server -IncludeAllSubFeature
+Install-WindowsFeature Web-Server -IncludeManagementTools
